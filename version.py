478c6073c002f3d4518dc6f7d5405e2e347ef0b5
973721777 <973721777@qq.com>
2026-05-02 11:48:07 +0800

    update .gitignore file.

--- version.py
+++ version.py
@@ -1,14 +1,6 @@
 # -*- coding: utf-8 -*-
-#
-# Copyright (c) 2024 DBCheck Contributors
-# sdfiyon@gmail.com
-#
-# This file is part of DBCheck, an open-source database health inspection tool.
-# DBCheck is released under the MIT License.
-# See LICENSE or visit https://opensource.org/licenses/MIT for full license text.
-#
 """
-DBCheck 版本集中管理
-每次发布只需修改 __version__ 的值，所有模块统一引用
+DBCheck 版本信息 (供静态网站读取)
+这个文件会被转换为 version.json 供 docs/index.html 使用
 """
 __version__ = "v2.3.8"
