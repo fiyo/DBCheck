742d168e19e88f111118e0a840d155ac8e777c4f
973721777 <973721777@qq.com>
2026-09-24 13:36:58 +0800

    chore(license): verify_pyz_kdf.py 补 Apache-2.0 许可头

--- scripts/verify_pyz_kdf.py
+++ scripts/verify_pyz_kdf.py
@@ -1,3 +1,7 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2025-2026 fiyo (Jack Ge) <sdfiyon@gmail.com>
+# Author: fiyo (Jack Ge) - https://github.com/fiyo/DBCheck
+
 """Verify cryptography kdf modules are packed inside a PyInstaller onefile exe (Issue #57)."""
 import struct, sys, zlib
 
